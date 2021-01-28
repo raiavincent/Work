775a9b8e70c5e45e4622e577ad8ff3e2e56ee205
Vincent Raia <vincentraia1@gmail.com>
2021-01-28 15:54:37 -0500

    come on gui

--- emailRegex.py
+++ emailRegex.py
@@ -1,31 +1,31 @@
 import pyperclip, re
 
 # okay so I made this a while loop and it ran continuously good night
-while True:
-    # Create email regex.
-    emailRegex = re.compile(r'''(
-        [a-zA-Z0-9._%+-]+ # username
-        @                 # @ symbol
-        [a-zA-Z0-9.-]+  # domain name
-        (\.[a-zA-Z]{2,4})
-        )''', re.VERBOSE)
 
-    # TODO: Find matches in clipboard text
-    # TODO: Copy results to the clipboard
+# Create email regex.
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @                 # @ symbol
+    [a-zA-Z0-9.-]+  # domain name
+    (\.[a-zA-Z]{2,4})
+    )''', re.VERBOSE)
 
-    # Find matches in the clipboard text
-    text = str(pyperclip.paste())
+# TODO: Find matches in clipboard text
+# TODO: Copy results to the clipboard
 
-    matches = []
-    for groups in emailRegex.findall(text):
-        matches.append(groups[0])
+# Find matches in the clipboard text
+text = str(pyperclip.paste())
 
-    #TODO: Copy results to the clipboard.
+matches = []
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
-    # Copy results to the clipboard.
-    if len(matches) > 0:
-        pyperclip.copy('\n'.join(matches))
-        print('Copied to the clipboard:')
-        print('\n'.join(matches))
-    else:
-        print('No email addresses found')
+#TODO: Copy results to the clipboard.
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to the clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No email addresses found')
